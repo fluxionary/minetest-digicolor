bc4feeede33d8b4af8c9001bc8406da0a52d5913
flux <25628292+fluxionary@users.noreply.github.com>
2022-09-01 15:37:50 -0700

    specify craft output

--- craft.lua
+++ craft.lua
@@ -1,5 +1,5 @@
 minetest.register_craft({
-	output = "",
+	output = "digicolor:node",
 	recipe = {
 		{"group:color_white", "digilines:lcd", "group:color_black"},
 		{"group:color_red", "group:color_green", "group:color_blue"},
